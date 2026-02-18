a503b9c9ebd8536164f2213ffe41943a0a155104
magurophone <jansho6402@yahoo.co.jp>
2026-02-19 03:09:20 +0900

    管理画面から設定を更新

--- public/config.js
+++ public/config.js
@@ -37,8 +37,8 @@ window.DASHBOARD_CONFIG = {
   "fonts": {
     "display": "'Sacramento', cursive",
     "displayUrl": "https://fonts.googleapis.com/css2?family=Sacramento:wght@400&display=swap",
-    "body": "'Hachi Maru Pop', cursive",
-    "bodyUrl": "https://fonts.googleapis.com/css2?family=Hachi%20Maru%20Pop:wght@400&display=swap",
+    "body": "'M PLUS 2', sans-serif",
+    "bodyUrl": "https://fonts.googleapis.com/css2?family=M+PLUS+2:wght@400;500;700&display=swap",
     "googleFontsUrl": "https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700;900&display=swap"
   },
   "images": {
